faf8bd82316dcdc4e9bd4be4f312003e2cb72e1a
Théodore DANIEL <theodore.daniel@epitech.eu>
2022-10-17 17:37:06 +0200

    Adding numbers and priorities globals

--- includes/globals.h
+++ includes/globals.h
@@ -5,8 +5,10 @@
 #include "logger.h"
 
 extern const char *allowed_chars;
+extern const char *numbers;
 extern const char *operators;
 extern const char *operators_without_minus;
+extern const char priorities[2][4];
 
 extern const utils_t Utils;
 extern const logger_t Logger;
--- sources/globals.c
+++ sources/globals.c
@@ -1,12 +1,16 @@
 #include "../includes/globals.h"
 
 const char *allowed_chars = "0123456789+-/*\%()";
+const char *numbers = "0123456789";
 const char *operators = "+-/*\%";
 const char *operators_without_minus = "+/*\%";
+const char priorities[2][4] = {"*/\%\0", "+-\0"};
 
 const utils_t Utils = {
     contains,
     contains_other_than,
+    is_a_number,
+    is_not_a_number,
     is_in,
     is_not_in,
     count_occurence,
