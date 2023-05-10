10e73225fe74a5fb8d3e0601896d624a6d07b2c2
Ashmoore <theodore.daniel@epitech.eu>
2023-05-10 20:15:28 +0200

    Adding tests for utils

--- tests/sources/test_utils.c
+++ tests/sources/test_utils.c
@@ -1,7 +1,32 @@
 #include "null.h"
 #include "bool.h"
 #include <criterion/criterion.h>
+#include "globals.h"
 
-Test(simple, test) {
-    cr_assert_str_eq("0", "0");
+#include <stdio.h>
+#include <stdlib.h>
+
+Test(contains, simple_working) {
+    char str[] = "hello world";
+
+    cr_assert_str_eq(Utils.contains(str, "w"), "world");
+    cr_assert_str_eq(Utils.contains(str, "zpo"), "o world");
+}
+
+Test(contains, simple_uncorrect) {
+    char str[] = "hello world";
+
+    printf("%p\n", Utils.contains(str, "z"));
+    printf("%p\n", NULL);
+    cr_assert_str_eq(Utils.contains(str, "z"), NULL);
+}
+
+Test(str_reverse, simple_correct) {
+    char s[] = "hello world";
+    char *str = malloc(sizeof(char) * strlen(s));
+    strcpy(str, s);
+
+    Utils.str_reverse(str);
+    cr_assert_str_eq(str, "dlrow olleh");
+    free(str);
 }
